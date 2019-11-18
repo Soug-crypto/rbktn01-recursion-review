d1ad0524745d57b434cd407fd03c17061a1c3303
Ahmed Wheida <ahmed.wheida@gmail.com>
2019-11-18 20:01:58 +0100

    finished the basic exercises

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,23 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+
+	var acc = [];
+
+
+
+	for (var i = 0; i < document.body.childNodes.length; i++) {
+
+		if (document.body.childNodes[i].className === className){
+
+			acc.push(document.body.childNodes[i]);
+
+		}
+
+	}
+
+	return acc
+
+  
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,66 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
-};
+
+	if (obj === NaN || obj === null || obj === Infinity) return "null";
+
+	if (typeof obj === "string" ) return '"' + obj + '"';
+
+	if (typeof obj === "boolean" || typeof obj === "number"  ) return obj.toString();
+
+	if (Array.isArray(obj)) {
+
+		for (var i = 0; i < obj.length; i++) {
+			obj[i] = stringifyJSON(obj[i]);
+		}
+
+		return "["+obj.join(",")+ "]";
+
+	}
+
+	if (typeof obj === "object") {
+
+
+		if (Object.keys(obj).length === 0 ) return  '{}';
+
+
+		var acc = "{"
+
+		for (var key in obj){
+
+			if (obj[key]=== undefined){
+
+				return  '{}'
+
+			}
+
+			acc = acc + '"' +key.toString()+ '"' + ":" + stringifyJSON(obj[key])+',';
+
+		}
+
+		var lastC = acc[acc.length-1]
+
+		if (lastC === ',') {
+
+			acc = acc.substring(0,acc.length-1)
+
+			return acc + "}"
+		} else {
+
+			return acc + "}"
+
+		}
+
+
+
+		
+
+	}
+
+}
+
+
+
+ 
+
+
